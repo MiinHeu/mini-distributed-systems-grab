2b8bd44ccd79ca0ea015a0907ade06894af7f2f2
leeminh00 <ahehehihihahahuhu123@gmail.com>
2026-05-10 16:46:16 +0700

    Audit & Stabilization: Update backend/src/trips/trips.module.ts for production readiness

--- backend/src/trips/trips.module.ts
+++ backend/src/trips/trips.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpModule } from '@nestjs/axios';
 import { TripsController } from './trips.controller';
 import { TripsGateway } from './trips.gateway';
 import { TripsService } from './trips.service';
-import { Trip } from './entities/trip.entity';
 import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { DbRoutingModule } from '../db-routing/db-routing.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Trip]), AuthModule, DatabaseModule, HttpModule, DbRoutingModule],
+  imports: [AuthModule, DatabaseModule, HttpModule, DbRoutingModule],
   controllers: [TripsController],
   providers: [TripsService, TripsGateway],
   exports: [TripsService, TripsGateway],
